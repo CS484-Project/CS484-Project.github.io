d96c2e39beb71428b75e79f67eb57eeff7566b42
bensaal <108480998+bensaal@users.noreply.github.com>
2023-10-18 10:36:57 -0400

    added comments to important files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+/* This page initializes the rendering and preload functions that are built in
+from Electron.js. See below for description of methods */
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('node:path')
 
+/* Creates window utilizing preload script */
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -13,6 +16,7 @@ const createWindow = () => {
   win.loadFile('./src/index.html')
 }
 
+/* Creates window if no windows are loaded on start-up */
 app.whenReady().then(() => {
     ipcMain.handle('ping', () => 'pong')
   createWindow()
@@ -24,6 +28,7 @@ app.whenReady().then(() => {
   })
 })
 
+/* Quits app if all windows are closed */
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
