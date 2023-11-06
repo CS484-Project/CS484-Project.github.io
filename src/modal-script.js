1a9e85bc22ac0275fbb1a2efcc1c7b9badc93413
Derik Torres <deriktorres33@gmail.com>
2023-11-06 14:54:24 -0500

    food web fail

--- src/modal-script.js
+++ src/modal-script.js
@@ -1,11 +1,30 @@
 
-let webTable = {};
+/** Gather JSON data */
+import data from './animals.json' assert { type: "json" };
+console.log(data);
 
-function animal(namePar, infoPar, imgPar){
-    this.name = namePar;
-    this.info = infoPar;
-    this.img = imgPar;
-    webTable[namePar] = this;
+
+let modal = `
+    <div id="myModal" class="modal">
+
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="close">&times;</span>
+                <h2>Modal Header</h2>
+            </div>
+    
+            <div class="modal-body">
+                <p>Some text in the Modal Body</p>
+                <p>Some other text...</p>
+            </div>
+            <div class="modal-footer">
+                <h3>Modal Footer</h3>
+            </div>
+        </div>
+
+    </div>
+`
+
+async function callModal(id){
+    alert(id);
 }
-
-
